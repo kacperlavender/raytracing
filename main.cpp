75344f54eb98a039fc3108de928649c9eb7ee181
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-12-25 19:55:12 +0100

    c

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
-#include "color.h"
-#include "vec3.h"
-
 int main() {
 
     // image
@@ -10,17 +7,27 @@ int main() {
     int image_height = 256;
 
     // render
-
+	// p3 oznacza ze kolory sa w ascii; 256 za max color
     std::cout << "P3\n"
               << image_width << " " << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
-            write_color(std::cout, pixel_color);
+    for (int i = 0; i < image_height; i++) {
+        for (int j = 0; j < image_width; j++) {
+			// liczby z przedzialu [0, 1]
+			// np dla drugiej iteracji 1/255 
+			// po width dajemy od czerni do czerwonosci
+            auto r = double(j) / (image_width - 1);
+			// po height od czerni do zielonego
+            auto g = double(i) / (image_height - 1);
+            auto b = 0.0;
+
+			// rozciagamy liczby z [0, 1] do [0, 256]
+			// 255.99 dla pewnosci ze nie przekroczymy 256
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+
+			std::cout << ir << " " << ig << " " << ib << std::endl;
         }
     }
-
-    return 0;
 }
