74e3b9422f70fd48d034d756c632c7add20b8094
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-12-26 16:36:57 +0100

    c

--- main.cpp
+++ main.cpp
@@ -1,33 +1,55 @@
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
 #include <iostream>
 
+color ray_color(const ray& r){
+	vec3 unit_direction = unit_vector(r.direction());
+	auto a = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 int main() {
-
     // image
+	auto aspect_ratio = 16.0 / 9.0;
     int image_width = 256;
-    int image_height = 256;
 
-    // render
+	// ustawiamy image height nie losowo, tylko pod aspect_ratio
+	// jesli jest mniejsze niz 1; to to 1
+	int image_height = int (image_width / aspect_ratio);
+	image_height = (image_height < 1) ? 1 : image_height;
+
+	auto focal_length = 1.0;
+	auto viewport_height = 2.0;
+	auto viewport_width = viewport_height * (double(image_width)/image_height);
+	auto camera_center = point3(0,0,0);
+
+	// calculate the vectors across the horizontal and down the vertical viewport edges
+	auto viewport_u = vec3(viewport_width, 0, 0);
+	auto viewport_v = vec3(0, -viewport_height, 0);
+
+	// calculate the horizontal and vertical delta vectors from pixel to pixel
+	auto pixel_delta_u = viewport_u / image_width;
+	auto pixel_delta_v = viewport_v / image_height;
+
+	// calculate the location of the upper left pixel
+	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+	// render
 	// p3 oznacza ze kolory sa w ascii; 256 za max color
     std::cout << "P3\n"
               << image_width << " " << image_height << "\n255\n";
 
-    for (int i = 0; i < image_height; i++) {
-        for (int j = 0; j < image_width; j++) {
-			// liczby z przedzialu [0, 1]
-			// np dla drugiej iteracji 1/255 
-			// po width dajemy od czerni do czerwonosci
-            auto r = double(j) / (image_width - 1);
-			// po height od czerni do zielonego
-            auto g = double(i) / (image_height - 1);
-            auto b = 0.0;
+	for(int j = 0; j < image_height; j++){
+		for(auto i = 0; i < image_width; i++){
+			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_direction = pixel_center - camera_center;
+			ray r(camera_center, ray_direction);
 
-			// rozciagamy liczby z [0, 1] do [0, 256]
-			// 255.99 dla pewnosci ze nie przekroczymy 256
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
-
-			std::cout << ir << " " << ig << " " << ib << std::endl;
-        }
-    }
+			color pixel_color = ray_color(r);
+			write_color(std::cout, pixel_color);
+		}
+	}
 }
--- ray.h
+++ ray.h
@@ -3,22 +3,22 @@
 
 #include "vec3.h"
 
-class ray{
-	public:
-		ray() {}
+class ray {
+    point3 orig;
+    vec3 dir;
 
-		ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+    public: 
+        ray() {}
 
-		const point3& origin() const { return orig; }
-		const point3& directon() const { return dir; }
+        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
-		point3 at(double t) const {
-			return orig + t*dir;
-		}
+        const point3& origin() const { return orig; }
+        const vec3& direction() const { return dir; }
 
-	private:
-		point3 orig;
-		vec3 dir;
+        point3 at(double t) const {
+            return orig + t * dir;
+        }
+
 };
 
-#endif
+#endif 
